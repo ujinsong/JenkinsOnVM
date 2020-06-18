fd0a6d38e4a3e617390ff998c5a333903a1ed16e
Ubuntu <azadmin@vm01.yfwrufnlqmcevj2spau4h515ve.cx.internal.cloudapp.net>
2020-06-18 07:40:26 +0000

    second commit

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const app = express();
 var server = http.createServer(app);
 
 app.get('/', function(req, res){
-    res.send('I love Azure');
+    res.send('I love sangsong');
 });
 
 app.get('/start', function(req, res){
